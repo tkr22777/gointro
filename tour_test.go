17f2766672dbc78621fffd5e5b8b04691f5ff6c2
Tahsin Kabir <tahsinkabir@gmail.com>
2020-03-15 20:48:57 -0700

    Examples for type conversion, interference and constants

--- tour_test.go
+++ tour_test.go
@@ -7,6 +7,11 @@ import (
 	"testing"
 )
 
+const (
+	Big = 1 << 100
+	Small = Big >> 99
+)
+
 func TestEtc(t *testing.T) {
 	fmt.Println("A random number:", rand.Intn(10))
 	fmt.Println("Sqrt of 7:", math.Sqrt(7))
@@ -19,6 +24,9 @@ func TestEtc(t *testing.T) {
 	fmt.Println(varDeclaration3())
 	fmt.Println(varTypes())
 	fmt.Println(zeroValues())
+	fmt.Println(typeConversion())
+	fmt.Println(defaultTypeInference())
+	fmt.Println(constants())
 }
 
 func add(x int, y int) int {
@@ -73,6 +81,25 @@ func zeroValues() string {
 	return fmt.Sprint(i, f, b, s)
 }
 
+func typeConversion() string {
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x * x + y * y))
+	var z uint = uint(f)
+	return fmt.Sprint(x, y, z)
+}
+
+func defaultTypeInference() string {
+	i := 294967292 // int32
+	f := 3.142 //
+	g := 0.867 + 0.5i //complex128
+	return fmt.Sprintf("294967292 is of type:%T, 3.142 is of type:%T and 0.867 + 0.5i is of type:%T", i, f, g)
+}
+
+func constants() string {
+	const PI = 3.1416
+	const MORTAL = true
+	return fmt.Sprint(PI, MORTAL)
+}
 
 
 
