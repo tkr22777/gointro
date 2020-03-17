ed84b3807bcd60c019852e1c1aa20634b41ad7b1
Tahsin Kabir <tahsinkabir@gmail.com>
2020-03-16 19:23:09 -0700

    Added sample edit handler and go html templating code

--- wiki/wiki.go
+++ wiki/wiki.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"html/template"
 )
 
 type Page struct {
@@ -51,12 +52,8 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-		"<form action=\"/save/%s\" method=\"POST\">"+
-		"<textarea name=\"body\">%s</textarea><br>"+
-		"<input type=\"submit\" value=\"Save\">"+
-		"</form>",
-		p.Title, p.Title, p.Body)
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
 }
 
 func main() {
